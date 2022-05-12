8b5d35efa529e1fb3511ab119605215789eafef5
Javier Matilla Martin <59919628+jdejavi@users.noreply.github.com>
2022-05-12 17:51:31 +0200

    Script para eliminar y parar los servidores
    Script para eliminar y parar los servidores en los host

---
+++ parar_y_limpiar.sh
@@ -0,0 +1,17 @@
+#limpia cualquier carpeta con nombre $2 en la mAquina@usuario $1
+if [ $# -eq 1 ]
+then
+	host=$1
+	#path=$2
+	echo "[!] Borrando carpeta Bully del host $(hostname -I)"
+	rm -rf Bully/
+	echo "[!] Parando los servidores en el host $host"
+	ssh $host "./Escritorio/tomcat8080/bin/shutdown.sh; ./Escritorio/tomcat8081/bin/shutdown.sh; exit"
+	echo "[!] Servidores parados correctamente"
+	echo "[!] Borrando los servidores en el $host"
+	ssh $host "rm -rf /home/i3840766/Escritorio/tomcat8080;rm -rf /home/i3840766/Escritorio/tomcat8081; exit;"
+	echo "[!] Directorios limpiados satisfactoriamente"
+else
+	echo "[!] Uso: $0 [Usuario@Ip maquina]"
+	exit
+fi
